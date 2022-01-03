4523f3013e1892792691be7ca360a575790e40d1
nognsongosnovun <96969215+nognsongosnovun@users.noreply.github.com>
2022-01-03 10:31:29 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,54 @@
+const dictionary = {
+  "The" : {
+    "word" : "the",
+    "lexicalCategory":"Determiner",
+    "definition" : "denoting one or more people or things already mentioned or assumed to be common knowledge."
+  },
+  "India" : {
+    "word" : "India",
+    "lexicalCategory":"Noun",
+    "definition" :"a country in southern Asia occupying the greater part of the Indian subcontinent; population 1,311,000,000 (estimated 2015); official languages, Hindi and English (fourteen other languages are recognized as official in certain regions; of these, Bengali, Gujarati, Marathi, Tamil, Telugu, and Urdu have most first-language speakers); capital, New Delhi."
+  },
+  "Externally" : {
+    "word" : "externally",
+    "lexicalCategory":"Adverb",
+    "definition" : "with reference to the outer surface or structure of something; outside"
+  },
+  "Hello" : {
+    "word" : "hello",
+    "lexicalCategory":"Interjection",
+    "definition" : "used as a greeting or to begin a telephone conversation"
+  },
+  "Welcome" : {
+    "word" : "welcome",
+    "lexicalCategory":"Noun",
+    "definition" : "an instance or manner of greeting someone"
+  },
+  "Almost" : {
+    "word" : "almost",
+    "lexicalCategory":"Adverb",
+    "definition" : "not quite; very nearly"
+  },
+  "She" : {
+    "word" : "she",
+    "lexicalCategory":"Pronoun",
+    "definition" : "used to refer to a woman, girl, or female animal previously mentioned or easily identified"
+  },
+  "Tree" : {
+    "word" : "tree",
+    "lexicalCategory":"Noun",
+    "definition" : "a woody perennial plant, typically having a single stem or trunk growing to a considerable height and bearing lateral branches at some distance from the ground."
+  },
+  "Wonderful" : {
+    "word" : "wonderful",
+    "lexicalCategory":"Adjective",
+    "definition" : "inspiring delight, pleasure, or admiration; extremely good; marvellous"
+  },
+  "Ordinary" : {
+    "word" : "ordinary",
+    "lexicalCategory":"Adjective",
+    "definition" : "with no special or distinctive features; normal"
+  }
+}
+
+export default dictionary
